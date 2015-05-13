c9fb3f3b8748f44780fc6d47a5faa048068dc086
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-05-13 00:38:05 -0500

    add test image -> uncertainty seems to work!

--- experiemnts.m
+++ experiemnts.m
@@ -17,6 +17,7 @@ grid = meshgrid(1:width, 1:height);
 % based on curr versions of disparity, has to be updatet whenever disp are
 shiftedXIndices = grid - stereoImg.DisparityMap;
 
+
 window = Window;
 oldWin = window;
 x = 392;
@@ -33,6 +34,48 @@ colLeft = Window.expand(window, 4);
 
 wins = [colRigth, rowBelow, rowAbove, colRigth];
 for i = 1:4
-    uncerts(i) = uncertainty(stereoImg, x, y, wins(i));
+    %uncerts(i) = uncertainty(stereoImg, x, y, wins(i));
+    uncerts(i) = uncertainty(stereoImg, x, y, Window.expand(window,i));
 end
 uncerts
+
+
+%% Making a test image
+w = 70;
+h = 50;
+linGrad = meshgrid(1:w, 1:h); % linear gradient
+gradient1 = imnoise(linGrad/(size(linGrad,2)),'gaussian'); % add noise
+% make up ground truth disparity
+dispBox = 3*ones(h, w); % most things displaced by 3
+dispBox(15:h-15,20:w-20) = 7; % central piece is displaced by 7
+
+
+
+% from http://stackoverflow.com/questions/7132863/non-uniform-shifting-of-pixels
+%# create coordinate grid for image gradient1
+[xx,yy] = ndgrid(1:h,1:w);
+%# linearize the arrays, and add the offsets
+xx = xx(:);
+yy = yy(:);
+linDispBox = dispBox(:);
+B(:,2) = linDispBox;
+B(:,1) = 0;
+xxShifted = xx + B(:,1);
+yyShifted = yy + B(:,2);
+%# preassign C to the right size and interpolate
+gradient2  = gradient1;
+%gradient2(:) = interp2(xx,yy,gradient1(:),xxShifted,yy);
+gradient2(:) = griddata(xx,yy,gradient1(:),xxShifted,yyShifted);
+
+% initialize new stereo image set based in gradients
+testStImg = StereoImage(gradient1,gradient2);
+% cheat: get the actual dispairty ground truth with some noise
+testStImg.DisparityMap = dispBox + round(rand(h,w));
+
+x = 52; y = 20; % on the right edge of the center rectangle
+% estimate its uncert
+window = Window;
+uncert = uncertainty(testStImg, x, y, window);
+uncertLeft = uncertainty(testStImg, x, y, Window.expand(window, 4)); % big
+uncertRight = uncertainty(testStImg, x, y, Window.expand(window, 2)); % same
+%% conclusion: it kinda works!
